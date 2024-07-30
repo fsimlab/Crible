2dbf8a0a43e466e4da16e7bfb5ffcacdd182d4a0
Sylvain Desanghere <sylvain.desanghere@gmail.com>
2024-07-30 13:19:57 +0200

    Update Crible.R

--- Crible.R
+++ Crible.R
@@ -1,11 +1,10 @@
-
-liste=c(2,4,3,5)
-
+# Crible.R 
+# S. Desanghere - 2024
+liste=c(2,3,4,5)
 n=6
 while(n<5000){
 	if((n/2) %in% liste & !(n %in% liste)){liste=c(liste,n)}
 	if((n/3) %in% liste & !(n %in% liste)){liste=c(liste,n)}
 	if((n/5) %in% liste & !(n %in% liste)){liste=c(liste,n)}
 	n=n+1}
-	
 print(liste)
